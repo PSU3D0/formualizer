3794083d28e2a939164c68061f61caed474f4665
Jörg Beyer <joerg.beyer@gmail.com>
2026-02-28 10:11:24 +0100

    style: addressing clippy hints

--- crates/formualizer-workbook/tests/large_sheets.rs
+++ crates/formualizer-workbook/tests/large_sheets.rs
@@ -2,11 +2,9 @@ use formualizer_common::LiteralValue;
 use formualizer_workbook::Workbook;
 
 fn fill_col(wb: &mut Workbook, sheetname: &str, colnum: u32, start_rownum: u32, num_rows: u32) {
-    let mut val: i64 = 0;
-    for rownum in start_rownum..start_rownum + num_rows + 1 {
-        wb.set_value(sheetname, rownum, colnum, LiteralValue::Int(val))
+    for (val, rownum) in (start_rownum..start_rownum + num_rows + 1).enumerate() {
+        wb.set_value(sheetname, rownum, colnum, LiteralValue::Int(val as i64))
             .unwrap();
-        val += 1;
     }
 }
 
@@ -18,9 +16,9 @@ fn single_large_column() {
     wb.add_sheet(sheetname).unwrap();
     let n = 10_000;
     let expected_sum: f64 = (n * (n + 1) / 2) as f64;
-    fill_col(&mut wb, &sheetname, 2, 1000, n);
+    fill_col(&mut wb, sheetname, 2, 1000, n);
 
-    _ = wb.set_formula(sheetname, 1, 1, "=sum(B:B)").unwrap();
+    wb.set_formula(sheetname, 1, 1, "=sum(B:B)").unwrap();
     let result = wb.evaluate_cell(sheetname, 1, 1).unwrap();
 
     assert_eq!(result, LiteralValue::Number(expected_sum));
